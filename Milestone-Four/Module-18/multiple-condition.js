d62e5fd5478e6cda6a9fbd26c74f8b60262d5e4b
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 17:14:49 +0600

    Multiple Condition

---
+++ Milestone-Four/Module-18/multiple-condition.js
@@ -0,0 +1,45 @@
+var gotJob = true;
+var moneySaved = 150000;
+var hasFlat = false;
+var hasHouse = true;
+
+// if(gotJob == true && moneySaved > 200000){
+//     console.log('I got good job');
+    
+// }
+// else{
+//     console.log('I did not got job.');
+    
+// }
+
+
+
+// if(gotJob == true && moneySaved > 200000 && hasFlat == true){
+//     console.log('I got good job');
+    
+// }
+// else{
+//     console.log('I did not got job.');
+    
+// }
+
+
+
+// if(gotJob == true || moneySaved > 200000){
+//     console.log('I got good job');
+    
+// }
+// else{
+//     console.log('I did not got job.');
+    
+// }
+
+
+if((gotJob == true && moneySaved > 200000) || hasHouse == true){
+    console.log('I got good job');
+    
+}
+else{
+    console.log('I did not got job.');
+    
+}
