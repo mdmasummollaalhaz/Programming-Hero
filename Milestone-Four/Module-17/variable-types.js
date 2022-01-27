f53d5a85ae7a156bb53003f61d1e16a57a0ea47f
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 14:15:11 +0600

    Variable Types

---
+++ Milestone-Four/Module-17/variable-types.js
@@ -0,0 +1,27 @@
+var tShirtPrice = 200;
+console.log(typeof tShirtPrice);
+
+var price = "200";
+console.log(typeof price);
+
+var isRaining = true;
+console.log(typeof isRaining);
+
+
+var isRomantic ;
+console.log(typeof isRomantic);
+
+var today = new Date();
+console.log(typeof today);
+
+
+// Special edition
+var firstNumber = 0.1;
+var secondNumver = 0.2;
+var total = firstNumber + secondNumver;
+// console.log(total.toFixed(1));
+total = total.toFixed(2);
+total = parseFloat(total);
+console.log(total);
+
+
