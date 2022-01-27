2cdecd067c6450d6ba95915990d51386d2994e17
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 12:31:28 +0600

    Math operation

---
+++ Milestone-Four/Module-17/mathematical-operation.js
@@ -0,0 +1,25 @@
+var onionPrice = 43;
+var eggPrice = 10;
+var eggQuantity = 7;
+
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var eggPriceMultiplication = eggPrice * eggQuantity;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+// console.log(totalPrice);
+// console.log(priceDifference);
+// console.log(eggPriceMultiplication);
+
+
+var moneyAvalilable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvalilable / orangePrice;
+console.log(orangeQuantity);
+
+
+
+
+
