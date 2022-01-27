9c67932f6d742220c6b0bc216c5bfc22041a1d8c
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 16:44:37 +0600

    Comparison

---
+++ Milestone-Four/Module-18/comparison.js
@@ -0,0 +1,24 @@
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log( 5 == 6 );
+// console.log( 5 == 5 );
+// console.log( 5 != 6 );  // Is they are different
+
+
+var myLove = 99;
+var yourLove = 100;
+// console.log(myLove > yourLove);
+// console.log(myLove < yourLove);
+// console.log(myLove == yourLove);
+// console.log(myLove != yourLove);
+
+console.log(studyGood && goodPerson);
+console.log(isBanker || goodResult);
+
+
+
+
+
+
+
+
