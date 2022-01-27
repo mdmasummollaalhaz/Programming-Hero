5a09430907ee949d5576a8d1a1f543b7495c070c
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 14:53:40 +0600

    Array

---
+++ Milestone-Four/Module-18/array.js
@@ -0,0 +1,14 @@
+var friedsAge = [11, 13, 17];
+
+var eat = [34, 34, 24, 29];
+
+var girls = ['Mahi', 'Aysha', 'Rupa'];
+
+var oddnumber = [1, 3, 5, 7 ,9];
+
+var vowels = ['a', 'e', 'e'];
+
+// console.log(vowels.length);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
+
