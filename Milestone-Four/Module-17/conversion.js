9b257422e07085f8e1904e238b9acfdb6dc9bfbb
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 13:58:23 +0600

    Conversion

---
+++ Milestone-Four/Module-17/conversion.js
@@ -0,0 +1,33 @@
+var fistName = 'Jennie';
+var lastName = 'Kim';
+var fullName = fistName + ' ' + lastName;
+// console.log(fullName);
+
+var frist = 'Masum';
+var last = 'Molla';
+var name = frist + ' ' + last;
+// console.log(name);
+
+
+var onionPrice = '42';
+// var eggPrice = '41';
+var eggPrice = 41;
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+
+// Integer
+var sunglass = 3;
+
+//Float
+var price = 99.99;
+
+
+
+var onionPrice = '42';
+var eggPrice = '41.23';
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+// console.log(onionPriceNumber);
+console.log(eggPriceNumber + onionPriceNumber);
+
