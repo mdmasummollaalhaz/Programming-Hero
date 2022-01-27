d21aaab2626fd65a1724df37acd585a0228689c3
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 15:27:12 +0600

    Array Index

---
+++ Milestone-Four/Module-18/array-index.js
@@ -0,0 +1,21 @@
+var books = ['Hablu', 'Blood', 'battery', 'Alu']
+
+var batteryIndex = books.indexOf('battery');
+// console.log(batteryIndex);
+
+
+var numbers = [45, 23, 32, 32, 83];
+var index = numbers.indexOf(32)
+// var index = numbers.indexOf(132)
+console.log(index);
+
+
+var secondIndex = books[2];
+// console.log(secondIndex);
+
+
+//Replace Number
+numbers[1] = 13;
+console.log(numbers);
+
+
