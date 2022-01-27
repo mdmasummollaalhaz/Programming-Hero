52c59868fbcef6ca10909f37389d0dc0171edf1a
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 17:02:06 +0600

    Conditional

---
+++ Milestone-Four/Module-18/conditional.js
@@ -0,0 +1,34 @@
+var isFoodReady = true;
+
+if (isFoodReady == true){
+    // console.log('Mama! I will eat now.');
+    
+}
+
+var iphonePrice = 70000;
+var myBudget = 1333;
+
+if(iphonePrice < myBudget){
+    // console.log('Got iPhone');
+    
+}
+else{
+    // console.log('Error');
+}
+
+if (iphonePrice > myBudget){
+    // console.log('My phone is best');
+    
+}
+
+var chickenPrice = 180;
+var myMoney = 50;
+
+if(chickenPrice < myMoney){
+    console.log('Yes! I will eat.');
+    
+}
+else{
+    console.log('Will eat 7up');
+    
+}
