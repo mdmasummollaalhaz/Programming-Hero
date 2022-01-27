6c4f375a0ad7c6fc3cc9e310f2f7e598e2fd4001
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 14:39:04 +0600

    Variable

--- Milestone-Four/Module-18/variable.js
+++ Milestone-Four/Module-18/variable.js
@@ -1,1 +1,16 @@
+// Number
 var watchPrice = 120;
+watchPrice = 125;
+
+//String
+var userName = 'Karim';
+
+// Boolean
+var isExersive = true;
+
+// Integer
+1 2 3 4 5 112
+
+// Float
+2.44 133.23
+
