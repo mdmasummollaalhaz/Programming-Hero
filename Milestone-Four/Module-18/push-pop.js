50349dc2fdbfa9904b27c29fbd66a4f2f1c9ce64
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 15:46:23 +0600

    Push-Pop

---
+++ Milestone-Four/Module-18/push-pop.js
@@ -0,0 +1,35 @@
+// Add elements to an arry
+var lastBench = ['Akash', 'Robel', 'Salam'];
+lastBench.push('Karim', 'Rasel');
+lastBench.push('Tanvir');
+// console.log(lastBench);
+
+
+var friendsAge = [12, 34, 31, 32, 42,];
+// friendsAge.push(23);
+console.log(friendsAge);
+
+
+// Remove an element from an arry
+// var lastItem = friendsAge.pop();
+// friendsAge.pop();
+// friendsAge.shift()
+friendsAge.unshift(333);
+console.log(friendsAge);
+// console.log(lastItem);
+
+
+
+// pop(): Remove an item from the end of an array.
+// push(): Add items to the end of an array.
+// shift(): Remove an item from the beginning of an array.
+// unshift(): Add items to the beginning of an array.
+
+
+// Javascript remove first item from an array
+// Javascript arry add element in the beginning
+
+
+
+
+
