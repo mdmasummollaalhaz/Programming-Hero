27ff5cf06947d16275b678e3ed7c7345e67a9747
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-27 13:37:00 +0600

    math-shorthand

---
+++ Milestone-Four/Module-17/math-shorthand.js
@@ -0,0 +1,48 @@
+//RECAP
+var price = 31;
+var price2 = 10;
+var sum = price + price2;
+var difference = price2 - price;
+var multiplication = price * price2;
+var division = price / price2;
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(division);
+
+
+//
+var doublePrice = price2 * 20;
+var newPrice = price + 10;
+
+// price = price + 44;
+price += 44;
+
+// price2 = price2 - 2;
+price2 -= 2;
+
+console.log(doublePrice);
+console.log(newPrice);
+
+console.log(price);
+console.log(price2);
+
+
+//
+var age = 14;
+// age = age + 1;
+// age += 1;
+age ++;
+// console.log(age);
+
+
+var love = 100;
+// love = love - 1;
+// love -= 1;
+love --;
+console.log(love);
+
+
+
+
+
