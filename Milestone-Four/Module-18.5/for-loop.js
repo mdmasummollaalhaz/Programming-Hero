6ed28cbb63d2fbd61c2c4edd6bbb9e8d85ff1bcb
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-28 13:15:53 +0600

    For loop

---
+++ Milestone-Four/Module-18.5/for-loop.js
@@ -0,0 +1,31 @@
+// var roastGive = 0;
+
+// while(roastGive < 7){
+//     console.log('Roast den, Please!');
+//     roastGive++;
+// }
+
+
+// for(var roastGive = 0; roastGive < 7; roastGive++){
+//     console.log('Roast den, please!');
+//     console.log(roastGive);
+   
+// }
+
+// for (var i = 0; i <= 7; i++){
+//     console.log(i);
+    
+// }
+
+// Odd
+// for( var i = 1; i <= 20; i+=2){
+//     console.log(i);
+    
+// }
+
+
+//Event
+for (var i = 0; i <= 20; i+=2){
+    console.log(i);
+    
+}
