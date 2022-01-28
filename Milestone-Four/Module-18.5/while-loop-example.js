40664565e70677e276e9cf1d54b793f272028cf2
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-28 12:56:22 +0600

    While loop example

---
+++ Milestone-Four/Module-18.5/while-loop-example.js
@@ -0,0 +1,21 @@
+var number = 1; //Loop variable
+
+// while(number <= 20){
+//     console.log(number);
+//     number++; 
+// }
+
+// Odd Numbers
+// while(number <= 10 ){
+//     console.log(number);
+//     number = number + 2;
+    
+// }
+
+// Even Numbers
+number = 0;
+while(number <= 10){
+    console.log(number);
+    number = number + 2;
+    
+}
