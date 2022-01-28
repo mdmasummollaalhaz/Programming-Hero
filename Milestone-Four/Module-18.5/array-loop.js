2b6dfb40d0eb060dcfc00d858e49f29b76f60ac3
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-28 15:08:10 +0600

    Array loop

---
+++ Milestone-Four/Module-18.5/array-loop.js
@@ -0,0 +1,15 @@
+// var numbers = [34, 83, 82, 22, 42, 22, 43, 77];
+
+// for ( var i = 0; i < numbers.length; i++){
+//     var element = numbers[i]
+//     console.log(element);
+    
+// }
+
+
+var items = ['bottle', 'mobile', 'charger', 'laptop', 'tv', 'book'];
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+    
+}
---
+++ Milestone-Four/Module-18.5/summary.js
@@ -0,0 +1,12 @@
+// Basic while loop
+var i = 1;
+while(i <= 7){
+    console.log(i);
+    i++;
+}
+
+// Basic for loop
+for (var i = 1; i <= 7; i++){
+    console.log(i);
+    
+}
