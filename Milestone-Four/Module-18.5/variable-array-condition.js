aedbcabba5b6d3af0ccf9577aaf33c569cfe13d1
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-28 12:08:02 +0600

    Variable array condition

--- Milestone-Four/Module-18.5/variable-array-condition.js
+++ Milestone-Four/Module-18.5/variable-array-condition.js
@@ -1,2 +1,27 @@
 var bottleColor = "yellow";
 var waterQuantity = 1;
+var isFull = true;
+
+// Array
+var items = ['Bottle', 'mug', 'glass'];
+items.indexOf('thala'); //-1
+items.push('envelope');
+items.push('watch');
+items.pop();
+console.log(items);
+
+
+//Conditionals
+if(items.length >= 4){
+    console.log('You have to many stuff on your desk');
+    
+}
+else if(items.length == 4){
+    console.log('You only have one hali items');
+    
+}
+else{
+    console.log('Wow! You have a clean desk');
+    
+}
+
