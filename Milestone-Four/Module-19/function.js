59a3373bec8ba2c6810944f680e7b9a2fd909f36
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-29 20:51:52 +0600

    Function

---
+++ Milestone-Four/Module-19/function.js
@@ -0,0 +1,20 @@
+// Curly Brackets
+
+// 
+function startFan(){
+    console.log('walk towards the switch');
+    console.log('press the switch');
+    
+}
+startFan(); //Call the function
+
+
+console.log('Eat breatfast');
+console.log('Drank tea');
+console.log('Take a shower');
+startFan()
+startFan()
+startFan()
+
+
+
