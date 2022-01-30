de345f39aec261db9eeba74324ab46c0df04190b
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-30 14:20:45 +0600

    Parameter Update

--- Milestone-Four/Module-19/parameter.js
+++ Milestone-Four/Module-19/parameter.js
@@ -1,9 +1,17 @@
 function bringSingara(taka){
     console.log('Singara Price', taka);
     console.log('Mama singara den');
+    var singaraPrice = 10;
+    var singaraQuantity = taka / singaraPrice;
+    return singaraQuantity;
     
 }
 var money = 150;
 // bringSingara(100);
-bringSingara(money);
+// bringSingara(money);
+
+var singara = bringSingara(money);
+console.log('Total', singara);
+
+
 
