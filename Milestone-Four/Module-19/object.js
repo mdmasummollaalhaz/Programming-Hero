fddac883c0b981ec03492180d295adb413ee6443
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-30 14:55:22 +0600

    Object

---
+++ Milestone-Four/Module-19/object.js
@@ -0,0 +1,22 @@
+var student1 = {
+    id: 15,
+    name: 'Masum',
+    marks: 82,
+    phone: 029292,
+
+};
+
+var mobile = {
+    color: 'Black',
+    price: 190000,
+    screenSize: 5,
+    storage: '16GB',
+};
+
+var book = {
+    title: 'Start with why',
+    author: 'Simon',
+    price: 82,
+    pages: 828,
+    published: 'Partem'
+}
