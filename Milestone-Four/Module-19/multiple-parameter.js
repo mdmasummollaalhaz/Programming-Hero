44395be0b11307f9806e7c2c4e0c3fd308ae8a6d
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-30 14:44:29 +0600

    Multiple Parameter

---
+++ Milestone-Four/Module-19/multiple-parameter.js
@@ -0,0 +1,43 @@
+// Addition
+function addTwoNumbers(number1, number2){
+    console.log(number1, number2);
+    var total = number1 + number2;
+    return total;
+
+    
+}
+var firstNumber = 35;
+var secontNumber = 47;
+
+var result = addTwoNumbers(firstNumber, secontNumber);
+console.log('Addition valu:', result);
+
+
+// Multiplicatio
+function multiplyTowNumber(num1, num2){
+    var total = num1 * num2;
+    return total;
+}
+
+var result = multiplyTowNumber(5, 10);
+console.log('Multiplicatio value:',  result);
+
+
+
+//subtraction
+function subtractionTowNumber (sub1, sub2){
+    var total = sub1 - sub2;
+    return total;
+}
+
+var result = subtractionTowNumber(12, 10)
+console.log('Subtraction value:',  result);
+
+
+// Division
+function divisionTwoNumber ( div1, div2){
+    total = div1 / div2;
+    return total;
+}
+var result = divisionTwoNumber(20, 5);
+console.log('Division value:',  result);
