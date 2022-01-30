019f091e930ec77a5de307231745c7b02677a52d
Masum Mollo <freelancermasummolla@gmail.com>
2022-01-30 15:25:01 +0600

    Switch

---
+++ Milestone-Four/Module-19/switch.js
@@ -0,0 +1,40 @@
+var color = 'yellow';
+
+if(color == 'blue'){
+    console.log('Color is blue');
+    
+}
+else if (color == 'grean'){
+    console.log('Color is green');
+    
+}
+else if (color == 'yellow'){
+    console.log('Color is yellow');
+    
+}
+else if (color == 'sky'){
+    console.log('Color is sky');
+    
+}
+else {
+    console.log('Color is Black');
+    
+}
+
+
+
+switch(color){
+    case 'blue':
+        console.log('Color is Blue');
+        break;
+    case 'red':
+        console.log('Color is Red');
+        break;
+    case 'yellow':
+        console.log('Color is yellow');
+        break;
+    default:
+        console.log('Color is black');
+        
+        
+}
