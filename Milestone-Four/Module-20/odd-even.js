9a96e400623bce6f6dc56323226ab4c7685d63b8
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-01 14:27:51 +0600

    Module 21 & Mat js

--- Milestone-Four/Module-20/odd-even.js
+++ Milestone-Four/Module-20/odd-even.js
@@ -1,34 +1,42 @@
-// const number = 4;
-// const remainder = number % 2;
-// console.log(remainder == 0);
+// // const number = 4;
+// // const remainder = number % 2;
+// // console.log(remainder == 0);
 
 
-const number = 145;
-function isEven(number){
-    if(number % 2 == 0){
-        return true;
-    }
-    return false;
+// const number = 145;
+// function isEven(number){
+//     if(number % 2 == 0){
+//         return true;
+//     }
+//     return false;
+// }
+
+// const myNumber = 1121;
+// const isMyNumberEven = isEven(myNumber);
+// console.log('Result:', isMyNumberEven);
+
+
+// const herNumber = 1892;
+// const isHerNumberEvent = isEven(herNumber);
+// console.log('Result:',isHerNumberEvent);
+
+
+// // Is odd function
+// function isOdd(number){
+//     if (number %2 != 0){
+//         return true;
+//     }
+//     return false;
+// }
+
+// const isMyNumberOdd = isOdd(myNumber);
+// console.log('Result odd:', isMyNumberEven);
+
+
+
+
+
+for (let i = 10; i >=1; i--){
+    console.log(i);
+    
 }
-
-const myNumber = 1121;
-const isMyNumberEven = isEven(myNumber);
-console.log('Result Even:', isMyNumberEven);
-
-
-const herNumber = 1892;
-const isHerNumberEvent = isEven(herNumber);
-console.log('Result Even:',isHerNumberEvent);
-
-
-// Is odd function
-function isOdd(number){
-    if (number %2 != 0){
-        return true;
-    }
-    return false;
-}
-
-const isMyNumberOdd = isOdd(myNumber);
-console.log('Result odd:', isMyNumberEven);
-
---
+++ Milestone-Four/module-21/math.js
@@ -0,0 +1,18 @@
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+const myNumber = 19.4;
+// const output = Math.ceil(myNumber); //Increaging
+// const output = Math.floor(myNumber); 
+// const output = Math.round(myNumber); //Majamji
+
+// console.log(output);
+
+
+for (let i = 0; i <= 20; i++){
+    const output = Math.random() * 5;
+    const rounded = Math.round(output); 
+    console.log(rounded);
+}
+
