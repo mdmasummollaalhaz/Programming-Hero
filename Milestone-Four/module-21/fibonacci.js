73e4f1b71e332485a4fcf27decc868ca7d491021
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-03 20:54:50 +0600

    fibo Function

--- Milestone-Four/module-21/fibonacci.js
+++ Milestone-Four/module-21/fibonacci.js
@@ -11,7 +11,9 @@ nth = (n-1)th +  (n-2)th
 ith = (i-1)th + (i-2)th
 */
 
-const fibo =[0, 1]
+
+
+/* const fibo =[0, 1]
 for(let i = 2; i <= 20; i++){
     // nth = (n-1)th +  (n-2)th
     // ith = (i-1)th + (i-2)th
@@ -19,3 +21,34 @@ for(let i = 2; i <= 20; i++){
     fibo[i] =fibo[i-1] + fibo[i-2];
 }
 console.log(fibo);
+ */
+
+
+
+/* function fibonacciSeries(num){
+    const fibo = [0, 1];
+    for(let i = 2; i<=num; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
+const fiboSeries =fibonacciSeries(1);
+console.log(fiboSeries); */
+
+
+
+function fibonacciSeries(num){
+    if(typeof num != 'number'){
+        return 'Please give a number:';
+    }
+    if(num < 2){
+        return 'Please enter a positive number greater then 1'
+    }
+    const fibo = [0, 1];
+    for(let i = 2; i<=num; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
+const fiboSeries =fibonacciSeries(10);
+console.log(fiboSeries);
