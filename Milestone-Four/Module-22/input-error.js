d23c873ebe4ae6781a3ff8991173a6b4a2fe81ea
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-04 00:51:22 +0600

    inpur error

---
+++ Milestone-Four/Module-22/input-error.js
@@ -0,0 +1,17 @@
+function add(number1, number2){
+    const sum = number1 + number2;
+    return sum;
+}
+const firstTotal = add(33, 22);
+// console.log(firstTotal);
+const secondTotal = add(44, 24);
+// console.log(secondTotal);
+
+
+function multiply(num1, num2){
+    const result = num1 * num2;
+    return result;
+}
+const firstResult = multiply(4 , 5);
+console.log(firstResult);
+
