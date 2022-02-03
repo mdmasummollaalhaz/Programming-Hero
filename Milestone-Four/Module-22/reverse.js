c10bf26708961a602ccc312811142fdc8fcd297a
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-03 21:52:44 +0600

    Reverse

---
+++ Milestone-Four/Module-22/reverse.js
@@ -0,0 +1,13 @@
+const greetings = 'Hello, how are you?';
+
+function reverseString(text){
+    let reverse = '';
+    for(const letter of text){
+        console.log(letter);
+        reverse = reverse + letter;
+        // reverse = letter + reverse;
+        
+    }
+}
+const reversed = reverseString(greetings);
+console.log(reversed);
