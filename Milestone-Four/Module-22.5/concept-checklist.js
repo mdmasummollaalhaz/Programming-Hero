43e8c639902adff3fd933d73c194072e9152f27e
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-04 23:18:00 +0600

    problem solving

---
+++ Milestone-Four/Module-22.5/concept-checklist.js
@@ -0,0 +1,12 @@
+// write 3 variables (Number, string, boolean)
+// 2 variables using (let, const)
+// Simple Math operations() +, -, *, /, %
+// Comparison (if , else)
+// two conditions. case-1: fulfill both conditions. case-2: fulfill at least one condition
+// if-else
+// while loop 7 to 19 all numbers. Only display numbers including 7 to 19
+// declare an array. number of elements, update or change 4 position element . add or remove elements . check whether specific value exists in the array
+// use any for loop to display every elements of an array
+// You have an array of numbers. display only the numbers bigger that 80
+//write a function that takes three numbers and returns the multiplication of the three numbers
+//declare an object and change any property of that
---
+++ Milestone-Four/Module-22.5/problem-solving-checklist.js
@@ -0,0 +1,20 @@
+// 1. Conversation
+feetToInch
+
+// 2. conversion
+centimeterToMeter
+
+// 3. calculation
+pageRequirements
+
+// book1 has 100 pages
+// book2 has 200 pages
+// book3 has 300 pages
+
+
+// 4. friends
+bestFriend
+
+
+// 5. will stop the loop if the array has any nagative number and returns all the positive number before the nagative number
+onlyPossitive
