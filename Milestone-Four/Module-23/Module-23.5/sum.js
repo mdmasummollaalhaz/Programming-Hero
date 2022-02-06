d72915a7e37381a179e97a6b2ad2bfe17996df39
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-06 18:27:57 +0600

    sum

---
+++ Milestone-Four/Module-23/Module-23.5/sum.js
@@ -0,0 +1,26 @@
+/* let sum = 0;
+for(let i = 5; i>=1; i--){
+    sum = sum + i;
+}
+console.log(sum);
+ */
+
+
+function sum(i){
+    // return sum() + i;
+    console.log(i);
+    if(i==1){
+        return 1;
+    }
+    return i + sum(--i);
+}
+console.log(sum(5));
+
+
+// i + sum(4)
+// i + sum(--i)
+// 5 + sum(4)
+// 5 + 4 sum(3)
+// 5 + 4 + 3 + sum(2)
+// 5 + 4 + 3 + 2 + 1
+// 15
