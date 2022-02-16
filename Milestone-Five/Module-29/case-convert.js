160534de3bc00c81f91394ffd5c932084f2a8f3a
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-16 11:37:36 +0600

    case convert

---
+++ Milestone-Five/Module-29/case-convert.js
@@ -0,0 +1,15 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+const userInput = 'blackPinK';
+const saveUserName = 'blackPink';
+console.log(userInput.toLowerCase());
+
+if(userInput.toLocaleLowerCase == saveUserName.toLocaleLowerCase()){
+    console.log('user exists');
+    
+}
+else{
+    console.log('Error');
+    
+}
