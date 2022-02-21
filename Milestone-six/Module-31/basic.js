3f2b9101d6acb8685eda4c05ca5b364508b33675
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-21 10:21:45 +0600

    Milestone six

---
+++ Milestone-six/Module-31/basic.js
@@ -0,0 +1,61 @@
+// Variable
+var deposit = 400;
+// condition
+if(deposit > 500){
+
+}
+else if(deposit < 200){
+
+}
+else if(deposit == 500){
+
+}
+else if(deposit != 500){
+
+}
+else if( deposit >= 500){
+
+}
+else if(deposit <=500){
+
+}
+else{
+
+}
+
+// Array
+const numbers = [23, 33, 223, 44, 33];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.push(11);
+numbers[2] = 555;
+
+// Included whether 222 included in the array
+if(numbers.indexOf(222) != -1){
+
+}
+if(numbers.includes(222)){
+
+}
+
+// Loop
+// while, for
+for(const number of numbers){
+
+}
+
+// function
+function fullName(first, second){
+    const name = first + ' ' + second;
+}
+const person = fullName('Rahim', 'Ali');
+
+// object
+const bottle = {
+    color: 'red',
+    contain: 'water',
+    price: 200,
+}
+
+
+
