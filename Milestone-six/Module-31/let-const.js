895a0b988309ac4a26e0e891ff353eb0cb36b646
Masum Mollo <freelancermasummolla@gmail.com>
2022-02-21 11:34:37 +0600

    let & const

---
+++ Milestone-six/Module-31/let-const.js
@@ -0,0 +1,29 @@
+let balance = 1222;
+balance = 1333;
+
+const userName = 'are the best';
+const  weTogether = 'we' + ' ' + userName;
+console.log(weTogether);
+// userName = 'Update';
+
+
+const numbers = [22, 32, 23, 21, 44, 42];
+// numbers = [99, 134, 3];  //not allowed
+numbers.push(33);
+numbers[1] = 333;
+
+let sum = 0;
+for(let i = 0; i < numbers.length: i++){
+    const number = numbers[i];
+    console.log(number);
+    
+}
+
+const student = {
+    roll: 101;
+    name 'ali',
+    job: 'intern'
+};
+student.name = 'Hossain';
+
+
