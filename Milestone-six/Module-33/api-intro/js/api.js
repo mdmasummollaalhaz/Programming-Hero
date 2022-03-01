8c6526fd318bd87e30f70322acc68c7bca2e49ba
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-01 13:22:40 +0600

    load more data api

--- Milestone-six/Module-33/api-intro/js/api.js
+++ Milestone-six/Module-33/api-intro/js/api.js
@@ -1,7 +1,22 @@
 function loadData(){
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
-        .then(json => console.log(json.title))
+        .then(json => console.log(json.title));
 };
 
+function loadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data));
+}
 
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+// loadPosts();
+
+function displayUsers(data){
+    console.log(data);
+}
