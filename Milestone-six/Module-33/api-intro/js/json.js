1e37e3a7c068f95d29685cf9f604a2ef6f3829eb
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-01 12:53:28 +0600

    update foldering

--- Milestone-six/Module-33/api-intro/js/json.js
+++ Milestone-six/Module-33/api-intro/js/json.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>API</title>
-</head>
-<body>
-    <h1>Hello API</h1>
-
-
-    <script>
-        // JavaScript Object Notation
+// JavaScript Object Notation
         //JSON
         const user = {
             id: 11,
@@ -38,16 +25,7 @@
         }
         const shopStringified = JSON.stringify(shop);
         // console.log(shop);
-        console.log(shopStringified);
+        // console.log(shopStringified);
         const converted = JSON.parse(shopStringified);
-        console.log(converted);
-        console.log(converted.owner);
-        
-        
-
-
-
-
-    </script>
-</body>
-</html>
+        // console.log(converted);
+        // console.log(converted.owner);
