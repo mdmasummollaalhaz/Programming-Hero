23a134e0523fee81d0692e1d694a9315bf18680e
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-02 12:14:33 +0600

    error solved

--- Milestone-six/Module-33/API-examples/js/countries.js
+++ Milestone-six/Module-33/API-examples/js/countries.js
@@ -1,27 +1,27 @@
 const loadCountries = () => {
-    fetch('https://restcountries.eu/rest/v2/all')
+    fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then(data => displayCountries(data));
 }
 loadCountries();
 
-const displayCountries = countries => {
+const displayCountries = counties => {
     /*
     for(const country of counties){
-    console.log(counties);
+    console.log(country);
     }
     */
 
     const countriesDiv = document.getElementById('countries');
-    countries.forEach(country => {
-        // console.log(countries); 
+    counties.forEach(country => {
+        // console.log(country); 
         const div = document.createElement('div');
         div.classList.add('country');
 
         div.innerHTML=`
-        <h3>${country.name}</h3>
+        <h3>${country.name.common}</h3>
         <p>${country.capital}</p>
-        <button onclick="loadCountryByName('${country.name}')">Details</button>
+        <button onclick="loadCountryByName('${country.name.common}')">Details</button>
         `;
 
         /* const h3 = document.createElement('h3');
@@ -37,20 +37,20 @@ const displayCountries = countries => {
 
 
 const loadCountryByName = name => {
-    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    const url = `https://restcountries.com/v3.1/name/${name}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayCountryDetails(data[0]));
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]));
 
-    console.log(url);
+    // console.log(url);
     
 }
 const displayCountryDetails = country => {
     // console.log(country);
     const countryDiv = document.getElementById('country-detail');
     countryDiv.innerHTML = `
-    <h5>${country.name}</h5>
+    <h5>${country.name.common}</h5>
     <p>Population: ${country.population}</p>
-    <img width="200px" src="${country.flag}">
+    <img width="200px" src="${country.flags.png}">
     `;
 }
