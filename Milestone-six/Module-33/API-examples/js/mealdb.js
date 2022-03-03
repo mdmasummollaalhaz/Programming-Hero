3a453265bea4d3415d83044fa731053902fadebd
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-04 00:05:00 +0600

    show products

--- Milestone-six/Module-33/API-examples/js/mealdb.js
+++ Milestone-six/Module-33/API-examples/js/mealdb.js
@@ -9,6 +9,29 @@ const searchFood = () =>{
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals));
-    
+        .then(data => displaySearchResult(data.meals));
+
 }
+
+    const displaySearchResult = meals =>{
+        console.log(meals);
+        const seachResult = document.getElementById('search-result');
+        meals.forEach(meal => {
+            // console.log(meal);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+                </div>
+            </div>
+            `;
+            seachResult.appendChild(div);
+            
+        })
+        
+    }
+    
