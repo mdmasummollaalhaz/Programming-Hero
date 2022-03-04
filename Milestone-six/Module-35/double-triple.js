ff82253e22be27b0430383e90b694e964dc103ef
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-04 23:13:48 +0600

    double triple

---
+++ Milestone-six/Module-35/double-triple.js
@@ -0,0 +1,25 @@
+// const first = 2;
+const first = 1;
+// const second = 2;
+const second = true;
+if(first == second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+
+// More comparison
+/* const a = {name: 'ali'};
+const b = {name: 'ali'}; */
+const a = [];
+const b = [];
+if(a == b){
+    console.log('both are same');
+    
+}
+else{
+    console.log('they are not same');
+    
+}
