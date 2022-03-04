a32ce71801c93496039a5a91dcfb6d73c63f1840
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-04 15:14:57 +0600

    Display data

--- Milestone-six/Module-33/API-examples/js/mealdb.js
+++ Milestone-six/Module-33/API-examples/js/mealdb.js
@@ -1,7 +1,7 @@
 const searchFood = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
 
     searchField.value = '';
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
@@ -14,14 +14,14 @@ const searchFood = () =>{
 }
 
     const displaySearchResult = meals =>{
-        console.log(meals);
+        // console.log(meals);
         const seachResult = document.getElementById('search-result');
         meals.forEach(meal => {
             // console.log(meal);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-            <div class="card">
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
@@ -33,5 +33,31 @@ const searchFood = () =>{
             
         })
         
+    }
+
+    const loadMealDetail = mealId => {
+        // console.log(mealId);
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?i=${mealId}`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayMealDetail(data.meals[0]));
+        
+    }
+
+    const displayMealDetail = meal => {
+        // console.log(meal);
+        const mealDetails = document.getElementById('meal-details');
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML= `
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 190)}</p>
+            <a href="${meal.strYoutube}" class="btn btn-primary">Youtube</a>
+            </div>
+        `;
+        mealDetails.appendChild(div);
     }
     
