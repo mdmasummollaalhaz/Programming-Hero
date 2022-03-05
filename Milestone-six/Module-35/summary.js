a9d4eca23e39d44f89261f7cc0945a6185725f42
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-05 12:44:47 +0600

    summary

---
+++ Milestone-six/Module-35/summary.js
@@ -0,0 +1,34 @@
+const a = 'Alim hossain';
+const b = 23;
+const c = true;
+// null
+// undefined
+
+// primitive
+// object
+
+const nums = [23, 44, 22, 53, 52];
+// console.log(typeof nums);
+// Array.isArray(nums)
+
+function triple(x, y, z){
+    x.age= 111;
+    // y = 222;
+    // z = 333;
+}
+// console.log(typeof triple);
+// console.log(triple.length);
+const num1 = 3;
+const num2 = 5;
+const num3 = 7;
+// triple(num1, num2, num3)
+const myObj = {name: 'Ali', age: 22};
+triple(myObj);
+console.log(myObj);
+
+console.log(num1, num2, num3);
+
+// console.log(typeof null);
+
+
+
