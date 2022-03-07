a23ca32aa4b3d2330223d2bc21ccd32ef13b1d52
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-07 19:23:51 +0600

    closure

---
+++ Milestone-six/module-37.5/closure.js
@@ -0,0 +1,27 @@
+const bank = owner =>{
+    balance = 0;
+    return {
+        deposit:amount =>{
+            // balance = balance + amount;
+            balance += amount;
+            return balance;
+        },
+        withdraw:amount =>{
+            // balance = balance - amount;
+            balance -= amount;
+            return balance;
+        },
+
+    }
+}
+
+const mofijBank = bank('Mofij');
+console.log(mofijBank.deposit(100));
+console.log(mofijBank.deposit(300));
+console.log(mofijBank.deposit(50));
+console.log(mofijBank.balance);
+console.log(mofijBank.deposit(200));
+console.log(mofijBank.withdraw(200));
+console.log(mofijBank.withdraw(100));
+
+
