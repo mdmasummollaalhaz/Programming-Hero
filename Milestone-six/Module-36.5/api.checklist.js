9a594273e3bd719f0ffe1f0934143ec29aa9afb5
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-07 15:07:57 +0600

    module 36.5

---
+++ Milestone-six/Module-36.5/api.checklist.js
@@ -0,0 +1,12 @@
+/* 
+1. object with property (string, boolean, number, object, array, method which wikk retune a property value)
+2. template string. minium e 3 property will be dynamic ( nested object property, array second element, another property)
+3.1: no perameter arrow function. return 89
+3.2: single paramenter arrow function, will divide input by 7
+3.3: arrow function with two parameters
+3.4: multline arrow function. two parameter.
+
+4. map divide each element by 7
+5. map, forEach, filter, find
+6. desctructuring on object and array
+*/
