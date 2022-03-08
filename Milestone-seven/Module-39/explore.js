1d63df255c75d97901b64c217b15fb18193cc1bf
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-08 20:53:43 +0600

    Module 39

---
+++ Milestone-seven/Module-39/explore.js
@@ -0,0 +1,16 @@
+const name = 15;
+const student = {
+    name: 'Ali',
+    friend: 'Karib',
+    job: 'Business',
+    makeMovie: function(name){
+        console.log(name)
+    }
+};
+student.makeMovie('The start');
+
+
+// Web APIs
+// document.getElementById('name');
+// fetch
+
