99156a01e0eff4c69cd55c673303d5833b148a75
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-09 12:36:50 +0600

    summary

---
+++ Milestone-seven/Module-39/summary.js
@@ -0,0 +1,20 @@
+aleart('');
+confirm('are you single?');
+prompt('name');
+
+
+// location.reload()
+// location.href = ''
+// location.hash = 
+
+// http://127.0.0.1:5500/Milestone-seven/Module-39/shopping.html
+
+// history.back()
+// history.go()
+
+// cookies
+
+// sessionStorage vs localStorage
+// cookies vs sessionStorage vs localStorage
+
+// document.body.contentEditable = true
