7c8d1afc49192c5458c5a6a7676960cb73812dee
Masum Mollo <freelancermasummolla@gmail.com>
2022-03-11 01:22:19 +0600

    summary

---
+++ Milestone-seven/Module-42/summary.js
@@ -0,0 +1,61 @@
+/* 
+1.1 variable (let, const)  three types variable
+1.2 condition (<,>, ===, !==, <=, =>, &&, ||, if-else )
+1.3 array: declare, length, index, push, pop, indexOf, includes
+1.4 for loop, while
+1.5 function return parameter
+1.6 object property, including array, object
+
+
+--------------------
+ES6
+1. template string ${}
+2. spread (...)
+2.1 copy and array then add a new element to an array
+2.2 use filter to remove an element from an array
+3. Arrow function
+3.1 no parameter - ()
+3.2 singla parameter
+3.3 multiple parameters ()
+3.4 multi line
+4. destructuring: object destructuring array destructiong
+5. object shorthand
+6. function parameter default value
+7. Optional chaining ( ?. )
+-----------------------------
+
+Browser API
+1. local storage session storage
+2. location API
+3. History API
+4. fetch
+-----------------------------
+
+Others
+1. array: map, forEach, filter, find
+2. ternary operator
+3. logical and logical or && ||
+4. JSON ( stringify, parse)
+
+*/
+
+
+const x = 50;
+const y = 100;
+const obj = {
+    x : x,
+    y : y
+};
+
+// Shortcut
+const obj1 = {x, y}
+
+const name = 'Ali Hossain';
+const area = 'Dhaka';
+
+const nobab = {
+    name: name,
+    area: area
+}
+
+const nobab2 = {name, area};
